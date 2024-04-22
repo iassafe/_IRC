85a8874c2237bb45d8b2dc8d79bdabade9f6af63
iassafe <ikrameassafe17@gmail.com>
2024-04-22 19:05:29 +0100

    add file kick.cpp

--- Commands.cpp
+++ Commands.cpp
@@ -73,12 +73,6 @@ int countComma(std::string str){
 	return (count);
 }
 
-
-void Server::execKickCommand(void){
-	if (send(this->connectionID, "Valid Command\n", 14, 0) == -1)
-		throw (std::runtime_error("failed to send to client"));
-}
-
 std::string	skipSpaces(std::string str){
 	size_t i;
 	for (i = 0; str[i] == ' '; ++i){
@@ -86,80 +80,6 @@ std::string	skipSpaces(std::string str){
 	return (&str[i]);
 }
 
-void Server::makeClientKick(std::string clKick, int exist2Points){
-	std::vector<std::string> vec;
-	clKick = skipSpaces(clKick);
-	size_t countClient = 0;
-	if (!exist2Points)
-		countClient = countComma(clKick);
-	if (!countClient)
-		vec.push_back(clKick);
-	else{
-		size_t found_comma = clKick.find_first_of(",");
-		vec.push_back(clKick.substr(0, found_comma));
-		clKick = clKick.substr(found_comma + 1, clKick.length());
-		for(size_t i = 0; i < countClient; ++i){
-			found_comma = clKick.find_first_of(",");
-			if (found_comma != std::string::npos){
-				vec.push_back(clKick.substr(0, found_comma));
-				clKick = clKick.substr(found_comma + 1, clKick.length());
-			}
-			else
-				vec.push_back(clKick.substr(0, clKick.length()));
-		}
-	}
-	for(size_t i = 0; i < vec.size(); ++i){
-		if (vec[i] != "")
-			this->ClientsKick.push_back(vec[i]);
-	}
-	vec.clear();
-}
-
-int Server::validArgsKick(void){
-	this->args = skipSpaces(this->args);
-	int count_args = 1;
-	for(size_t i =0; i < this->args.length(); ++i){
-		if (this->args[i] == ' '){
-			for(; this->args[i] == ' '; ++i){}
-			if (this->args[i])
-				count_args++;
-		}
-	}
-	if (count_args < 2)
-		return(0);
-	size_t found = this->args.find_first_of(" \t\r");
-	std::string temp_args = this->args;
-	this->Channelkick = temp_args.substr(0, found);
-	temp_args = temp_args.substr(found, temp_args.length());
-	int exist2Points = 0;
-	temp_args = skipSpaces(temp_args);
-	if (temp_args[0] == ':'){
-		temp_args = temp_args.substr(1, temp_args.length());
-		exist2Points = 1;
-	}
-	size_t found_sp = temp_args.find_first_of(" \t\r");
-	if (found_sp != std::string::npos){
-		temp_args = skipSpaces(temp_args);
-		if(!exist2Points)
-			temp_args = temp_args.substr(0, found_sp);
-		else
-			temp_args = temp_args.substr(0, temp_args.length());
-	}
-	makeClientKick(temp_args, exist2Points);
-	return (1);
-}
-
-void Server::kickCommand(Client &c){
-	this->args = skipSpaces(this->args);
-	if (this->args == "")
-		sendMsg(c.getClientFD(), ERR_NEEDMOREPARAMS(c.getNickname(), "KICK"));
-	else{
-		if(validArgsKick())
-				execKickCommand();
-		else
-			sendMsg(c.getClientFD(), ERR_NEEDMOREPARAMS(c.getNickname(), "KICK"));
-	}
-}
 
 void Server::joinCommand(Client &c){
 	this->existPassword = 0;
--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: iassafe <iassafe@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/01 18:16:43 by khanhayf          #+#    #+#              #
-#    Updated: 2024/04/22 13:04:44 by iassafe          ###   ########.fr        #
+#    Updated: 2024/04/22 19:04:17 by iassafe          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@
 NAME	= ircserv
 
 SRCS	= main.cpp Server.cpp Client.cpp authentication.cpp Channel.cpp \
-		invite.cpp mode.cpp bot.cpp Commands.cpp join.cpp topic.cpp
+		invite.cpp mode.cpp bot.cpp Commands.cpp join.cpp topic.cpp kick.cpp
 
 OBJS	= $(SRCS:.cpp=.o)
 
