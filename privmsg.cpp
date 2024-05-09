0c2511bc6033fe6cf5da1caf38709a2be07fea9e
iassafe <ikrameassafe17@gmail.com>
2024-05-09 15:58:37 +0100

    merge with AZMARA

--- privmsg.cpp
+++ privmsg.cpp
@@ -6,7 +6,6 @@ int	ft_count(std::string str, size_t start){
 			if (str[i] == ','){
 				for (t = i; str[t] == ','; t++)
 				i = t;
-				std::cout << "where the i is-" << str[t] << "-after skipping consecutive commas\n";
 				count++;
 			}
 		}
@@ -36,29 +35,22 @@ int	Server::validArgsPriv(std::string &args, Client &cli){
 				if (ind - start == 0)
 					break ;
 				this->vec_ch.push_back(args.substr(start, ind - start));
-				std::cout << "vc_ch" << vec_ch[i] << "$$$$$$$$" << std::endl;
 			}
 			else{
 				if (ind - start == 0){
 					puts("uuuu");
 					break ;
 				}
-				std::cout << "vc_cl" << args.substr(start, ind - start) << "#########@" << std::endl;
 				this->vec_cl.push_back(args.substr(start, ind - start));
 
 			}
-			// std::cout << "args[ind]"<< args[ind] << "------args[ind+1]" << args[ind+1] << "--------------------\n";
 			if (args[ind] == ' ')
 				break ;
 			start = ind + 1;
 			if (args[start] == ',')
 				for (; args[start] == ','; ++start){};
-			// std::cout << "args[start]" << &args[start]<< "-----------------------\n";
 			
 			ind = args.find_first_of(", ", start);
-			// if (args[ind] == ' ')
-			// 	break ;
-			std::cout << "________________________ind__[" << &args[ind] << "]\n";
 			if (ind == std::string::npos){
 				ind = args.find_first_of(" \t\r");
 				size_t k = ind;
@@ -69,25 +61,20 @@ int	Server::validArgsPriv(std::string &args, Client &cli){
 				if (ind == std::string::npos || args[k] == '\0'){
 					vec_ch.clear();
 					vec_cl.clear();
-				std::cout << "________________________ind__[" << args[k] << "]\n";
 					return (2);
 				}
 			}
 		}
 	}
 	size_t comma = index;
-		std::cout << "comma" << comma << "----" << args[comma] << "^^^^^^^^^^\n"; 
 	size_t msg_begin = (args.find_last_of(" \t\r"));
-	//GET THE MESSAGE AFTER ":" IN CASE OF MANY RECEPIENTS
 	if (args[index] == ','){
 		comma = ind;
 		for(; (args[comma] == ' ' || args[comma] == '\r' || args[comma] == '\t'); comma++)
-			std::cout << "args[comma]" << args[comma + 2] << "--------------********//////********" << std::endl;
 
 		if (args[comma] == ':'){
 			this->message = (args.substr(comma + 1 , args.size()));
 			isMessage = true;
-			std::cout << "ismessage" << isMessage << "-------)" << std::endl;
 		}
 		for (size_t	M = 0; M < vec_cl.size(); ++M){
 			if ((isInUseNickname(vec_cl[M]) == true)){
@@ -126,17 +113,12 @@ int	Server::validArgsPriv(std::string &args, Client &cli){
 			this->message = (args.substr(comma + 1 , args.size()));
 
 		}
-		std::cout << "index in case of one element" << args[index] << "-----------------------------\n";
 		this->target = args.substr(q, index - q);
 		if (isMessage == false)
 			this->message = (args.substr(msg_begin + 1, args.size()));//TODO: NEED TO GET ONLY FIRST PART//max 150 characters?
-		std::cout << "target[0]:" << this->target << "------+++++" << std::endl;
-		std::cout << "message in case of one element:" << this->message << "------+++++" << std::endl;
 		if (this->target[0] == '#'){
-			std::cout << ":return dial isInUseChName(this->target)" << isInUseChName(this->target) << "&&&&&&&//////" << std::endl;
 			if (isInUseChName(this->target) == true){
 				Channel	&chan = findChannel(this->target);
-				std::cout << "chan" << chan.getName() << "----------*" << std::endl;
 				if (chan.isMember(cli)){
 					chan.sendmsg2chanOperators(*this, this->message);
 					chan.sendmsg2chanRegulars(*this, this->message);
@@ -146,13 +128,10 @@ int	Server::validArgsPriv(std::string &args, Client &cli){
 				
 				return (1);
 			}
-			else{
-				std::cout << "NO SUCH CHANNEL" << cli.getNickname() << "33333333333333333333\n";
+			else
 				sendMsg(cli.getClientFD(), ERR_NOSUCHCHANNEL(cli.getNickname(), this->target));
-			}
 		}
 		else if (this->target[q] != '#'){
-			std::cout << "isInuse------------" << isInUseNickname(this->target) << "------------" << std::endl;
 			if (isInUseNickname(this->target) == true){
 				sendMsg(findClient(this->target).getClientFD(), this->message);
 				sendMsg(findClient(this->target).getClientFD(), "\n");
@@ -164,8 +143,6 @@ int	Server::validArgsPriv(std::string &args, Client &cli){
 
 		
 	}
-	std::cout << "this->target:" << this->target << "---;" << std::endl;
-		std::cout << "this->message:" << this->message << "---;" << std::endl;
 	return (1);
 }
 
@@ -180,8 +157,4 @@ void	Server::privmsgCommand(std::string &args, Client &cli){
 		sendMsg(cli.getClientFD(), ERR_NOSUCHNICK(cli.getNickname(), this->target));
 	}
 	
-	
-	// else if(validArgsPriv(args) == 8){
-	// 	sendMsg(cli.getClientFD(), "\n");
-	// }
 }
