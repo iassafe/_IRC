332ffd34ba95ba03929c53bc22403ea54f46cfbe
iassafe <ikrameassafe17@gmail.com>
2024-05-19 17:50:01 +0100

    last

--- privmsg.cpp
+++ privmsg.cpp
@@ -110,7 +110,7 @@ int	Server::validArgsPriv(std::string &args, Client &cli){
 	}
 	else{
 		for(; (args[comma] == ' ' || args[comma] == '\r' || args[comma] == '\t'); comma++){
-		};
+		}
 		if (args[comma] == ':'){
 			isMessage = true;
 			this->message = (args.substr(comma + 1 , args.size()));
