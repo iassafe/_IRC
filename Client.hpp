7af65498cf99e58c39cec201518b169d55b91960
iassafe <ikrameassafe17@gmail.com>
2024-05-20 18:13:23 +0100

    c

--- Client.hpp
+++ Client.hpp
@@ -6,7 +6,7 @@
 /*   By: iassafe <iassafe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 18:16:57 by khanhayf          #+#    #+#             */
-/*   Updated: 2024/05/19 19:11:13 by iassafe          ###   ########.fr       */
+/*   Updated: 2024/05/20 18:12:40 by iassafe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,16 +43,16 @@ class	Client{
         Client& operator=(Client const& obj);
         ~Client();
 
-		void	setBuffer(std::string rec);
-		void	setClientFD(int fd);
-        void    setNickname(std::string nn);
-        void    setUsername(std::string un);
-        void    setHostname(std::string hn);
-        void    setServername(std::string sn);
-        void    setRealname(std::string rn);
-        void    setRegistered(bool b);
-        void    setPasswordSended(bool b);
-        void    setClientIP(std::string ip);
+		void	    setBuffer(std::string rec);
+		void	    setClientFD(int fd);
+        void        setNickname(std::string nn);
+        void        setUsername(std::string un);
+        void        setHostname(std::string hn);
+        void        setServername(std::string sn);
+        void        setRealname(std::string rn);
+        void        setRegistered(bool b);
+        void        setPasswordSended(bool b);
+        void        setClientIP(std::string ip);
 
         int         getClientFD();
         std::string getNickname() const;
@@ -61,18 +61,17 @@ class	Client{
         std::string getServername() const;
         std::string getRealname() const;
         std::string getBuffer() const;
+        std::string getClientIP() const;
         bool		isRegistered() const;
         bool		isPasswordSended();
-        std::string getClientIP() const;
 
-        void            clearAuthentication();
-		void            registerClient(Server &s);
-        void            invite2channel(std::string chName);
-        bool            isInUseInvitedCh(std::string ChannelName);
-        std::string     tolowercase(std::string str);
-        void            removeInvitedCh(std::string ChannelName);
-
-        void clearBuffer();
+        void        clearAuthentication();
+		void        registerClient(Server &s);
+        void        invite2channel(std::string chName);
+        bool        isInUseInvitedCh(std::string ChannelName);
+        std::string tolowercase(std::string str);
+        void        removeInvitedCh(std::string ChannelName);
+        void        clearBuffer();
 
 };
 
